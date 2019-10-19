d6047bc8543d0e9c80656e6eefa5299c42856acb
Anthony <anthony@kobl.co>
2019-10-19 15:43:25 +1100

    Add Time/Data Filter

--- function.py
+++ function.py
@@ -1,15 +1,20 @@
 import json
 import csv
 import requests
+import io
+import datetime
 
 
 def lambda_handler(event, context):
-    url = 'https: // firms.modaps.eosdis.nasa.gov/data/active_fire/viirs/csv/VNP14IMGTDL_NRT_Global_24h.csv'
-    data = requests.get(url)
-
-    print('-=' * 40)
-    print(data)
-    print('-=' * 40)
+    now = datetime.datetime.now()
+    url = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/viirs/csv/VNP14IMGTDL_NRT_Global_24h.csv'
+    response = requests.get(url)
+    reader = csv.DictReader(io.StringIO(response.text))
+    for row in reader:
+        if (now - datetime.timedelta(hours=12)) < (datetime.datetime(int(row['acq_date'][0:4:]), int(row['acq_date'][5:7:]), int(row['acq_date'][8:10:]), int(
+            row['acq_time'][0:2:]), int(row['acq_time'][2:4:]))):
+            print(row)
+            break
 
     return {
         'statusCode': 200,

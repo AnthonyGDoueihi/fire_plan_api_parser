5a724fc4cf2979cd364b05044d591ada66f6fdcd
Anthony <anthony@kobl.co>
2019-10-19 16:03:52 +1100

    Add Distance from Lat Lng

--- function.py
+++ function.py
@@ -3,7 +3,20 @@ import csv
 import requests
 import io
 import datetime
+from math import sin, cos, sqrt, atan2, radians
 
+def get_distance_from_latLng(lat1, lat2, lng1, lng2):
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+    lng1 = radians(lng1)
+    lng2 = radians(lng2)
+    deltaLat = lat1 - lat2
+    deltaLng = lng1 - lng2
+
+    a = sin(deltaLat / 2)**2 + cos(lat1) * \
+        cos(lat2) * sin(deltaLng / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return R * c
 
 def lambda_handler(event, context):
     now = datetime.datetime.now()
@@ -13,7 +26,8 @@ def lambda_handler(event, context):
     for row in reader:
         if (now - datetime.timedelta(hours=12)) < (datetime.datetime(int(row['acq_date'][0:4:]), int(row['acq_date'][5:7:]), int(row['acq_date'][8:10:]), int(
             row['acq_time'][0:2:]), int(row['acq_time'][2:4:]))):
-            print(row)
+            distance = get_distance_from_latLng(1, float(row['latitude']), -50, float(row['longitude']))
+            print(distance)
             break
 
     return {
